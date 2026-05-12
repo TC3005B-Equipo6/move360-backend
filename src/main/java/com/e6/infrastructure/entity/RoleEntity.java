7c324bfc4273d623e2d0b156ead6668f3f725066
doyDynamo <dho2000@hotmail.com>
2026-05-12 13:01:20 -0600

    fix: update model and entity for user and role

---
+++ src/main/java/com/e6/infrastructure/entity/RoleEntity.java
@@ -0,0 +1,39 @@
+package com.e6.infrastructure.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "role")
+public class RoleEntity {
+
+    @Id
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    public RoleEntity() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
