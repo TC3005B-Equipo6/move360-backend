bfd1fb1f7c875d43c417247ee6014939d903c18b
doyDynamo <dho2000@hotmail.com>
2026-05-26 16:23:47 -0600

    fix: return id when creating dashboard

--- src/main/java/com/e6/application/dto/dashboard/CreateDashboardResponseDTO.java
+++ src/main/java/com/e6/application/dto/dashboard/CreateDashboardResponseDTO.java
@@ -2,9 +2,12 @@ package com.e6.application.dto.dashboard;
 
 import com.e6.domain.model.Dashboard;
 
-public record CreateDashboardResponseDTO(String title, String description, String user) {
+import java.util.UUID;
+
+public record CreateDashboardResponseDTO(UUID id, String title, String description, String user) {
     public static CreateDashboardResponseDTO from(Dashboard dashboard) {
         return new CreateDashboardResponseDTO(
+                dashboard.getId(),
                 dashboard.getTitle(),
                 dashboard.getDescription(),
                 dashboard.getOwner().getFirstName() + dashboard.getOwner().getPaternalSurname()
