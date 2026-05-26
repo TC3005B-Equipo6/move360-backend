df6cd17da409b69ceee6ce93d57a201d3f2713df
doyDynamo <dho2000@hotmail.com>
2026-05-26 16:29:06 -0600

    fix: return full name in UserProfileResponseDTO

--- src/main/java/com/e6/application/dto/auth/UserProfileResponseDTO.java
+++ src/main/java/com/e6/application/dto/auth/UserProfileResponseDTO.java
@@ -2,12 +2,13 @@ package com.e6.application.dto.auth;
 
 import com.e6.domain.model.User;
 
-public record UserProfileResponseDTO(String firstName, String surname, String role, String email) {
+public record UserProfileResponseDTO(String firstName, String paternalSurname, String maternalSurname, String role, String email) {
 
     public static UserProfileResponseDTO from(User user) {
         return new UserProfileResponseDTO(
                 user.getFirstName(),
                 user.getPaternalSurname(),
+                user.getMaternalSurname(),
                 user.getRole().getName(),
                 user.getEmail()
         );
