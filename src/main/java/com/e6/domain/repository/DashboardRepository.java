746e4eeb501822cbf38be93564f2cb6537a6c2aa
doyDynamo <dho2000@hotmail.com>
2026-05-26 12:33:26 -0600

    feat: add Tag methods to DashboardRepository

--- src/main/java/com/e6/domain/repository/DashboardRepository.java
+++ src/main/java/com/e6/domain/repository/DashboardRepository.java
@@ -1,5 +1,9 @@
 package com.e6.domain.repository;
 
+import com.e6.application.dto.dashboard.AddTagResponseDTO;
+import com.e6.application.dto.dashboard.CreateDashboardResponseDTO;
+import com.e6.application.dto.dashboard.GetDashboardsResponseDTO;
+import com.e6.application.dto.dashboard.GetUserDashboardsResponseDTO;
 import com.e6.domain.model.Dashboard;
 import com.e6.domain.model.User;
 
@@ -7,8 +11,17 @@ import java.util.List;
 import java.util.UUID;
 
 public interface DashboardRepository {
-    Dashboard createDashboard(Dashboard dashboard);
-    List<Dashboard> getUserDashboards(User user);
+    CreateDashboardResponseDTO createDashboard(Dashboard dashboard);
+
+    List<GetDashboardsResponseDTO> getPublicDashboards();
+
+    List<GetUserDashboardsResponseDTO> getUserDashboards(User user);
+
     Dashboard findDashboardById(UUID uuid);
+
     void deleteDashboardById(UUID id);
+
+    AddTagResponseDTO addTag(UUID id, int tagId);
+
+    void removeTag(UUID id, int tagId);
 }
